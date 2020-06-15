e388be0b6a7d9e60764caf2f9fbda1517d15d8df
Yu-chuan <newdavidlin@gmail.com>
2020-06-15 21:21:32 +0800

    Change DB

--- Database_Project/settings.py
+++ Database_Project/settings.py
@@ -77,14 +77,8 @@ WSGI_APPLICATION = 'Database_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        'USER': 'yuchuan',
-        'PASSWORD': '12345678',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
